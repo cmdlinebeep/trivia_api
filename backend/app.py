690a8577145ae48bb0dff0127c50571d40a9640d
cmdlinebeep <joelwigton@gmail.com>
2020-05-02 21:50:36 -0600

    Categories API route working and with unit tests.

---
+++ backend/app.py
@@ -0,0 +1,133 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import random
+
+from models import setup_db, Question, Category
+
+QUESTIONS_PER_PAGE = 10
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+
+    # Sets up CORS to allow '*' for origins on all API routes and resources.
+    # I changed the frontend routes to include /api/ in the path to make this
+    # a little more secure by being less permissive than the broadest strokes (CORS(app))
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    # Set Access-Control-Allow headers after each request
+    # (after_request decorator runs after the route handler for a request, but 
+    # keep in mind it MODIFIES the response.
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
+
+
+    '''
+    @TODO: 
+    Create an endpoint to handle GET requests 
+    for all available categories.
+    '''
+    @app.route('/api/categories')
+    def get_categories():
+        categories = Category.query.all()
+
+        cat_dict = {}
+        for cat in categories:
+            cat_dict[cat.id] = cat.type
+
+        return jsonify({
+            "success": True,
+            "categories": cat_dict
+        })
+
+
+    '''
+    @TODO: 
+    Create an endpoint to handle GET requests for questions, 
+    including pagination (every 10 questions). 
+    This endpoint should return a list of questions, 
+    number of total questions, current category, categories. 
+
+    TEST: At this point, when you start the application
+    you should see questions and categories generated,
+    ten questions per page and pagination at the bottom of the screen for three pages.
+    Clicking on the page numbers should update the questions. 
+    '''
+
+    '''
+    @TODO: 
+    Create an endpoint to DELETE question using a question ID. 
+
+    TEST: When you click the trash icon next to a question, the question will be removed.
+    This removal will persist in the database and when you refresh the page. 
+    '''
+
+    '''
+    @TODO: 
+    Create an endpoint to POST a new question, 
+    which will require the question and answer text, 
+    category, and difficulty score.
+
+    TEST: When you submit a question on the "Add" tab, 
+    the form will clear and the question will appear at the end of the last page
+    of the questions list in the "List" tab.  
+    '''
+
+    '''
+    @TODO: 
+    Create a POST endpoint to get questions based on a search term. 
+    It should return any questions for whom the search term 
+    is a substring of the question. 
+
+    TEST: Search by any phrase. The questions list will update to include 
+    only question that include that string within their question. 
+    Try using the word "title" to start. 
+    '''
+
+    '''
+    @TODO: 
+    Create a GET endpoint to get questions based on category. 
+
+    TEST: In the "List" tab / main screen, clicking on one of the 
+    categories in the left column will cause only questions of that 
+    category to be shown. 
+    '''
+    '''
+    @TODO: 
+    Create a POST endpoint to get questions to play the quiz. 
+    This endpoint should take category and previous question parameters 
+    and return a random questions within the given category, 
+    if provided, and that is not one of the previous questions. 
+
+    TEST: In the "Play" tab, after a user selects "All" or a category,
+    one question at a time is displayed, the user is allowed to answer
+    and shown whether they were correct or not. 
+    '''
+
+    '''
+    @TODO: 
+    Create error handlers for all expected errors 
+    including 404 and 422. 
+    '''
+
+    return app
+
+#----------------------------------------------------------------------------#
+# Launch.
+#----------------------------------------------------------------------------#
+
+# Default port:
+if __name__ == '__main__':
+    app.run()
+
+# Or specify port manually:
+'''
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -3,7 +3,7 @@ import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 
-from flaskr import create_app
+from app import create_app
 from models import setup_db, Question, Category
 
 
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format('postgres:a@localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +33,16 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories(self):
+        """Gets the /api/categories endpoint and checks valid results"""
+        res = self.client().get('/api/categories')
+        data = json.loads(res.data)
+
+        print(res)
+        print(data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(len(data['categories']), 6)
 
 
 # Make the tests conveniently executable
